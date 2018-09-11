b0d8077bc020df931fb6ddb2f5f329ce580cf924
eddiechapman <eddie.chapman4@gmail.com>
2018-09-11 17:33:14 -0500

    create list of sample info dicts

--- audio_sample_tools.py
+++ audio_sample_tools.py
@@ -7,91 +7,63 @@
 # in a text file for future renaming.
 
 
-import os
-import csv
+import os.path
 
-import pathlib
-from subprocess import Popen, PIPE
+
+ROOT = '/home/eddie/Music/Samples/JULEZ JADON DRUMS'
+
+def parse_directory(ROOT):
+    sample_info = []
+    for directory, sub_directories, files in os.walk(ROOT):
+        for file in files:
+            if file.endswith('.wav'):
+                sample = {
+                    'file': file,
+                    'filename': file.split('.')[0],
+                    'filetype': file.split('.')[1],
+                    'kit': directory.split('/')[-2],
+                    'sub_kit': directory.split('/')[-1]
+                }
+                sample_info.append(sample)
+    return sample_info
+
+
+def extract_filename_info(sample):
+    sample['filename_info'] = sample['filename'].split('_')
+
+
+def categorize_sound(sample):
+    pass
+
+
+def determine_if_loop(sample):
+    pass
+
+
+def find_bpm(sample):
+    pass
+
+def find_effect(sample):
+    pass
+
+def find_filter(sample):
+    pass
+
+def find_key(sample):
+    pass
 
 
 def main():
+    sample_info = parse_directory(ROOT)
+    for sample in sample_info:
+        extract_filename_info(sample)
+        categorize_sound(sample)
+        determine_if_loop(sample)
+        find_bpm(sample)
+        find_effect(sample)
+        find_filter(sample)
+        find_key(sample)
+        print(sample)
 
-    root = 'julez_jadon_drums'
-    path = os.path.join(root, '/home/eddie/Music/Samples')
-    sample_info = []
-    categories = set()
-
-    # TODO: first create list of (dirpath, filename) to then iterate over
-
-    for dirpaths, dirnames, filenames in os.walk(path):
-        os.chdir(dirpaths)
-        wav_files = [file for file in filenames if file.endswith('.wav')]
-        wav_files.sort()
-        for i, file in enumerate(wav_files):
-            sample = {}
-            file_bits = file.split('_')
-            path_bits = dirpaths.split('/')
-
-            index = str(i)
-            # pads zeros to left of index number
-            sample['sample_id'] = index.zfill(5)
-
-            sample['filename'] = file
-
-            sample['kit'] = path_bits[5]
-
-            sample['subdir'] = path_bits[6]
-
-            sample['category'] = file_bits[0]
-            categories.add(file_bits[0])
-
-            sample['name'] = file_bits[1]
-
-            if 'wet' or 'Wet' or 'FX' or 'Delay' in file_bits[2:]:
-                sample['effect'] = 'Wet'
-            elif 'dry' or 'Dry' in file_bits[2:]:
-                sample['effect'] = 'Dry'
-
-            if 'filter' or 'Filter' in file_bits[2:]:
-                sample['filter'] = 'Filter'
-
-            if 'BPM' in file_bits:
-                sample['loop'] = 'True'
-            else:
-                sample['loop'] = 'False'
-
-            if 'L' in file_bits[2:]:
-                sample['rl'] = 'L'
-
-            elif 'R' in file_bits[2:]:
-                sample['rl'] = 'R'
-
-            sample_info.append(sample)
-    cat_list = [cat for cat in categories]
-    cat_list.sort()
-    for cat in cat_list:
-        print(cat)
-
-
-
-
-    with open('sample_info.csv', 'w') as outfile:
-        os.chdir('/home/eddie/Music/Samples')
-        writer = csv.writer(outfile)
-        writer.writerows(sample_info)
-        print(os.path.abspath('sample_info.csv'))
-        print(len('sample_info.csv'))
 main()
-    # for
-    #
-    # p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    # p.stdout.close()
-    # p.wait()
 
-
-# def convert_bit_depth(file):
-#     for f in file:
-#          sox stuff
-#
-#             print
-#             os.path.join(root, file)
